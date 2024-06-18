c02d9d8fbaa39ae45c3203fa2d712b1a87a0cb4a
Airat Latypov <airat696@yandex.ru>
2024-06-19 02:58:24 +0400

    feat(chapter_05 files): creating pool of connections

--- chapter_05/create_connection_pool_5_7.py
+++ chapter_05/create_connection_pool_5_7.py
@@ -1,3 +1,36 @@
-"""
-Кэшированные подключения к бд называются пулом подключений.
-"""
+import asyncio
+
+from chapter_05.database import create_pool
+
+product_query = \
+    """
+    SELECT 
+    p.product_id, 
+    p.product_name, 
+    p.brand_id, 
+    s.sku_id,
+    pc.product_color_name,  
+    ps.product_size_name
+    FROM product as p
+    JOIN sku as s ON p.product_id = s.product_id
+    JOIN product_color as pc ON s.product_color_id = pc.product_color_id
+    JOIN product_size as ps ON s.product_size_id = ps.product_size_id
+    WHERE p.product_id = 100
+    """
+
+
+async def query_product(pool):
+    async with pool.acquire() as connection:
+        return await connection.fetchrow(product_query)
+
+
+async def main():
+    """
+    Кэшированные подключения к бд называются пулом подключений.
+    """
+
+    created_pool = await create_pool()
+    async with created_pool as pool:
+        await asyncio.gather(query_product(pool), query_product(pool))
+
+asyncio.run(main())
--- chapter_05/database.py
+++ chapter_05/database.py
@@ -1,6 +1,8 @@
 import asyncpg
 import os
 
+from asyncpg import Pool
+
 
 async def create_connection():
     connection = await asyncpg.connect(
@@ -11,3 +13,16 @@ async def create_connection():
         database=os.getenv("PRODUCTS_DB_NAME")
     )
     return connection
+
+
+async def create_pool(min_size=6, max_size=6) -> Pool:
+    pool: Pool = await asyncpg.create_pool(
+        host=os.getenv("DATABASE_HOST"),
+        port=os.getenv("DATABASE_PORT"),
+        user=os.getenv("DATABASE_USER"),
+        password=os.getenv("DATABASE_PASSWORD"),
+        database=os.getenv("PRODUCTS_DB_NAME"),
+        min_size=min_size,
+        max_size=max_size,
+    )
+    return pool
