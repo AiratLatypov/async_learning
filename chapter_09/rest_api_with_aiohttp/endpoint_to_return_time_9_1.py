aab83606df64a2f4bbd5c34aebe3a8ccb5908bd4
Airat Latypov <airat696@yandex.ru>
2024-07-19 01:26:43 +0400

    feat(chapter_09 files): aiohttp apis

---
+++ chapter_09/rest_api_with_aiohttp/endpoint_to_return_time_9_1.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from aiohttp import web
+from aiohttp.web_request import Request
+from aiohttp.web_response import Response
+
+routes = web.RouteTableDef()
+
+
+@routes.get("/time")
+async def time(request: Request) -> Response:
+    print("REQUEST", request.__dict__)
+    today = datetime.today()
+
+    result = {
+        "month": today.month,
+        "day": today.day,
+        "time": str(today.time())
+    }
+
+    return web.json_response(result)
+
+
+app = web.Application()
+app.add_routes(routes)
+web.run_app(app)
