648feb33d577b9d3569423d49904c8c24fe12562
Airat Latypov <airat696@yandex.ru>
2024-08-06 01:05:40 +0400

    feat(chapter_10 files): microservices files

--- chapter_10/microservices/main.py
+++ chapter_10/microservices/main.py
@@ -2,6 +2,7 @@ from aiohttp import web
 
 from chapter_05.database import add_database_pool_to_app, close_app_database_pool
 from chapter_10.microservices.stock_availability_service_10_1 import inventory_routes
+from chapter_10.microservices.favorites_service_10_5 import favorites_routes
 
 
 app = web.Application()
@@ -9,6 +10,7 @@ app.on_startup.append(add_database_pool_to_app)
 app.on_cleanup.append(close_app_database_pool)
 
 app.add_routes(inventory_routes)
+app.add_routes(favorites_routes)
 
 
 if __name__ == "__main__":
