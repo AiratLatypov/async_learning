ff158edea4eebbfe2ab0133c4b6bbac8ab78dac2
Airat Latypov <airat696@yandex.ru>
2024-08-06 17:43:37 +0400

    feat(chapter_10 files): microservices files

--- chapter_10/microservices/main.py
+++ chapter_10/microservices/main.py
@@ -5,6 +5,7 @@ from chapter_10.microservices.stock_availability_service_10_1 import inventory_r
 from chapter_10.microservices.favorites_service_10_5 import favorites_routes
 from chapter_10.microservices.cart_service_10_6 import cart_routes
 from chapter_10.microservices.products_service_10_7 import products_routes
+from chapter_10.microservices.backend_for_frontend_products_10_8 import all_products_routes
 
 
 app = web.Application()
@@ -15,6 +16,7 @@ app.add_routes(inventory_routes)
 app.add_routes(favorites_routes)
 app.add_routes(cart_routes)
 app.add_routes(products_routes)
+app.add_routes(all_products_routes)
 
 
 if __name__ == "__main__":
